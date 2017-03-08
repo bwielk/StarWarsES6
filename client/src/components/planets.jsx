1d9d7da19925948bc33909ea1268eabd45c35ba1
Blazej <bwielk@gmail.com>
2017-03-08 22:00:00 +0000

    API loads up

--- client/src/components/planets.jsx
+++ client/src/components/planets.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 
 class Planets extends React.Component{
-  render(){
-    return(
-      <p>Planets page</p>
-      )
-  }
+  constructor(props){
+     super(props);
+     this.state = {planets: []};
+   }
+
+   componentDidMount(){
+     this.loadPlanets("http://swapi.co/api/planets/");
+   }
+
+   loadCharacters(url){
+     const request = new XMLHttpRequest();
+     request.open('GET', url);
+     request.onload = () => {
+       if(request.status !== 200) return;
+       const jsonString = request.responseText;
+       const data = JSON.parse(jsonString);
+       console.log(data);
+       this.setState({planets: data})
+     }
+     request.send();
+   }
+
+   render(){
+     var planetMap = this.state.planets.map(function(planet, index){
+       return (
+       <p key = {index} className = "character-box" >
+       <li>{planet.name}</li>
+       </p>
+       );
+     });
+
+     return(
+       <div className = "all-characters">
+       <p>Characters page</p>
+       <ol>
+       {planetMap}
+       </ol>
+       </div>
+       )
+   }
 };
 
 export default Planets;
--- client/src/components/starships.jsx
+++ client/src/components/starships.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 
 class Starships extends React.Component{
-  render(){
-    return(
-      <p>Starships page</p>
-      )
-  }
+
+  constructor(props){
+     super(props);
+     this.state = {starships: []};
+   }
+
+   componentDidMount(){
+     this.loadStarships("http://swapi.co/api/starships/");
+   }
+
+   loadCharacters(url){
+     const request = new XMLHttpRequest();
+     request.open('GET', url);
+     request.onload = () => {
+       if(request.status !== 200) return;
+       const jsonString = request.responseText;
+       const data = JSON.parse(jsonString);
+       console.log(data);
+       this.setState({starships: data})
+     }
+     request.send();
+   }
+
+   render(){
+     var shipMap = this.state.starships.map(function(starship, index){
+       return (
+       <p key = {index} className = "starship-box" >
+       <li>{starship.name}</li>
+       </p>
+       );
+     });
+
+     return(
+       <div className = "all-starships">
+       <p>Characters page</p>
+       <ol>
+       {shipMap}
+       </ol>
+       </div>
+       )
+   }
 };
 
 export default Starships;
